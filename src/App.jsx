9adf50c0dfc1db39ca79add68b5988291fa6b792
guerkankaraduman-prog <guerkan.karaduman@gmail.com>
2026-05-12 18:20:04 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import ProductGrid from "./components/ProductGrid";
 import Navbar from "./components/Navbar";
-import DailyPicksHero from "./components/DailyPicksHero";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <>
       <Navbar />
-
-      <DailyPicksHero />
-
+      <Hero />
       <ProductGrid />
 
       <section
