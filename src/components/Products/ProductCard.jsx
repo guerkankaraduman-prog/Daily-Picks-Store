69ad92feefe9719fdf7cd19b885bc074bb9532f0
guerkankaraduman-prog <guerkan.karaduman@gmail.com>
2026-05-13 08:27:19 +0200

    Update ProductCard.jsx

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -1,1 +1,34 @@
+export default function ProductCard({ product }) {
+  return (
+    <div className="product-card">
 
+      <div className="product-image-wrapper">
+        <img
+          src={product.image}
+          alt={product.title}
+          className="product-image"
+        />
+      </div>
+
+      <div className="product-content">
+
+        <h3>{product.title}</h3>
+
+        <p className="product-price">
+          {product.price}
+        </p>
+
+        <a
+          href="https://amazon.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="product-button"
+        >
+          View Product ↗
+        </a>
+
+      </div>
+
+    </div>
+  );
+}
