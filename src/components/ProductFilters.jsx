fb267091eab4bd3c66ddbfb4fc152a4ad01a1486
guerkankaraduman-prog <guerkan.karaduman@gmail.com>
2026-05-13 08:49:40 +0200

    Create ProductFilters.jsx

---
+++ src/components/ProductFilters.jsx
@@ -0,0 +1,34 @@
+export default function ProductFilters({
+  activeCategory,
+  setActiveCategory,
+}) {
+  const categories = [
+    "All",
+    "Tech",
+    "Fitness",
+    "Home",
+    "Lifestyle",
+  ];
+
+  return (
+    <div className="product-filters">
+
+      {categories.map((category) => (
+        <button
+          key={category}
+          className={
+            activeCategory === category
+              ? "filter-button active"
+              : "filter-button"
+          }
+          onClick={() =>
+            setActiveCategory(category)
+          }
+        >
+          {category}
+        </button>
+      ))}
+
+    </div>
+  );
+}
