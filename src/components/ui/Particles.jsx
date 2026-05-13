2b8b90a9926098f44a3e7e2da1e1f849dd9caf22
guerkankaraduman-prog <guerkan.karaduman@gmail.com>
2026-05-13 09:31:31 +0200

    Create Particles.jsx

---
+++ src/components/ui/Particles.jsx
@@ -0,0 +1,38 @@
+export default function Particles() {
+
+  const particles = Array.from(
+    { length: 24 }
+  );
+
+  return (
+    <div className="particles">
+
+      {particles.map((_, index) => (
+
+        <span
+          key={index}
+          className="particle"
+
+          style={{
+            left:
+              `${Math.random() * 100}%`,
+
+            animationDelay:
+              `${Math.random() * 8}s`,
+
+            animationDuration:
+              `${8 + Math.random() * 12}s`,
+
+            width:
+              `${4 + Math.random() * 8}px`,
+
+            height:
+              `${4 + Math.random() * 8}px`,
+          }}
+        ></span>
+
+      ))}
+
+    </div>
+  );
+}
