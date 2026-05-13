f488bbd4aca53a9a80c2616e63f8e06869cf446d
guerkankaraduman-prog <guerkan.karaduman@gmail.com>
2026-05-13 08:32:02 +0200

    Update products.js

--- src/data/products.js
+++ src/data/products.js
@@ -1,11 +1,32 @@
 import Hero from './components/hero/Hero'
 
 const products = [
- {
-   id:1,
-   name:'JBL Flip 6',
-   price:'129€'
- }
-]
+  {
+    id: 1,
+    title: "Apple Watch Ultra",
+    price: "$799",
+    image:
+      "https://images.unsplash.com/photo-1546868871-7041f2a55e12",
+    link: "https://amazon.com",
+  },
 
-export default products
+  {
+    id: 2,
+    title: "Sony Headphones",
+    price: "$349",
+    image:
+      "https://images.unsplash.com/photo-1505740420928-5e560c06d30e",
+    link: "https://amazon.com",
+  },
+
+  {
+    id: 3,
+    title: "Gaming Setup",
+    price: "$1299",
+    image:
+      "https://images.unsplash.com/photo-1493711662062-fa541adb3fc8",
+    link: "https://amazon.com",
+  },
+];
+
+export default products;
